24ed2788728a7b77a7110dc6e6ad9af0e0e32f98
Anushka-2021 <annamilk2021@mail.ru>
2022-12-12 01:15:57 +0300

    Sessions added elementary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 import requests, base64, sqlite3, datetime
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -23,9 +23,28 @@ heads = {
 
 #langs = requests.request("GET", "https://judge0-ce.p.rapidapi.com/languages", headers = heads).json()
 langes = [{'id': 45, 'name': 'Assembly (NASM 2.14.02)'}, {'id': 46, 'name': 'Bash (5.0.0)'}, {'id': 47, 'name': 'Basic (FBC 1.07.1)'}, {'id': 75, 'name': 'C (Clang 7.0.1)'}, {'id': 76, 'name': 'C++ (Clang 7.0.1)'}, {'id': 48, 'name': 'C (GCC 7.4.0)'}, {'id': 52, 'name': 'C++ (GCC 7.4.0)'}, {'id': 49, 'name': 'C (GCC 8.3.0)'}, {'id': 53, 'name': 'C++ (GCC 8.3.0)'}, {'id': 50, 'name': 'C (GCC 9.2.0)'}, {'id': 54, 'name': 'C++ (GCC 9.2.0)'}, {'id': 86, 'name': 'Clojure (1.10.1)'}, {'id': 51, 'name': 'C# (Mono 6.6.0.161)'}, {'id': 77, 'name': 'COBOL (GnuCOBOL 2.2)'}, {'id': 55, 'name': 'Common Lisp (SBCL 2.0.0)'}, {'id': 56, 'name': 'D (DMD 2.089.1)'}, {'id': 57, 'name': 'Elixir (1.9.4)'}, {'id': 58, 'name': 'Erlang (OTP 22.2)'}, {'id': 44, 'name': 'Executable'}, {'id': 87, 'name': 'F# (.NET Core SDK 3.1.202)'}, {'id': 59, 'name': 'Fortran (GFortran 9.2.0)'}, {'id': 60, 'name': 'Go (1.13.5)'}, {'id': 88, 'name': 'Groovy (3.0.3)'}, {'id': 61, 'name': 'Haskell (GHC 8.8.1)'}, {'id': 62, 'name': 'Java (OpenJDK 13.0.1)'}, {'id': 63, 'name': 'JavaScript (Node.js 12.14.0)'}, {'id': 78, 'name': 'Kotlin (1.3.70)'}, {'id': 64, 'name': 'Lua (5.3.5)'}, {'id': 89, 'name': 'Multi-file program'}, {'id': 79, 'name': 'Objective-C (Clang 7.0.1)'}, {'id': 65, 'name': 'OCaml (4.09.0)'}, {'id': 66, 'name': 'Octave (5.1.0)'}, {'id': 67, 'name': 'Pascal (FPC 3.0.4)'}, {'id': 85, 'name': 'Perl (5.28.1)'}, {'id': 68, 'name': 'PHP (7.4.1)'}, {'id': 43, 'name': 'Plain Text'}, {'id': 69, 'name': 'Prolog (GNU Prolog 1.4.5)'}, {'id': 70, 'name': 'Python (2.7.17)'}, {'id': 71, 'name': 'Python (3.8.1)'}, {'id': 80, 'name': 'R (4.0.0)'}, {'id': 72, 'name': 'Ruby (2.7.0)'}, {'id': 73, 'name': 'Rust (1.40.0)'}, {'id': 81, 'name': 'Scala (2.13.2)'}, {'id': 82, 'name': 'SQL (SQLite 3.27.2)'}, {'id': 83, 'name': 'Swift (5.2.3)'}, {'id': 74, 'name': 'TypeScript (3.7.4)'}, {'id': 84, 'name': 'Visual Basic.Net (vbnc 0.0.0.5943)'}]
+app.secret_key = '1806363f0fmshf3926631702e101p1eda9ajsn5e10975d9be7'
+
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if 'username' not in session:
+            session['username'] = request.form['username']
+            return render_template('login.html', deta = 'Hello, ' + request.form['username'], logInfo = 'Logout')
+        else:
+            return render_template('login.html', deta = 'You are already in as ' + session['username'], logInfo = 'Logout')
+    else:
+        return render_template('login.html')
+
+@app.route('/logout', methods = ['GET', 'POST'])
+def logout():
+    session.pop('username', None)
+    return render_template('lk.html', logInfo = 'Login')
 
 @app.route('/person', methods = ['GET', 'POST'])
 def personal():
+    if 'username' not in session:
+        return render_template("lk.html", data = 'You are not in')
     cursor.execute("SELECT * FROM table572")
     noun = 0
     f = cursor.fetchone()
@@ -39,7 +58,7 @@ def personal_today():
     now = datetime.datetime.now()
     day = now.strftime("%d/%m/%y")
     todays_subs = []
-    cursor.execute("SELECT * FROM table572 WHERE day=?", (day,))
+    cursor.execute("SELECT lang, code, input, exp_output, output, time FROM table572 WHERE day=?", (day,))
     todays_subs.append(cursor.fetchall())
     print(todays_subs)
     if todays_subs is None:
@@ -60,7 +79,6 @@ def personal_today():
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        myCode = request.form.get('Code')
         myCode = request.form.get('code_area')
         mC = myCode.encode("UTF-8")
         Code = base64.b64encode(mC)
@@ -97,7 +115,7 @@ def index():
         time = now.strftime("%H:%M:%S")
         day = now.strftime("%d/%m/%y")
 
-        cursor.execute("INSERT OR REPLACE INTO table572(des_token, lang, code, input, output, time, day) VALUES (?, ?, ?, ?, ?, ?, ?)", (decision_tok_n, myCodeLanguage, Cd, myCin, rs, time, day))
+        cursor.execute("INSERT OR REPLACE INTO table572(des_token, lang, code, input, output, time, day) VALUES (?, ?, ?, ?, ?, ?, ?)", (decision_tok_n, myCodeLanguage, myCode, myCin, rs, time, day))
         conn.commit()
         noun = 0
         cursor.execute("SELECT * FROM table572")
